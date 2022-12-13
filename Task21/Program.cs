5933e8a96c8b86431c3edc7403a4fec02062856c
Aleksandr <bestprogreraxe@gmail.com>
2022-12-13 03:32:52 +0300

    solved Task21

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -4,3 +4,37 @@
 A (3,6,8); B (2,1,-7), -> 15.84
 A (7,-5, 0); B (1,-1,9) -> 11.53 */
 
+double GetNumber(string message)
+{
+    bool isCorrect = false;
+    double result = 0;
+    while (!isCorrect) 
+    {
+        Console.WriteLine(message);        
+        if (double.TryParse(Console.ReadLine(), out result))
+        {
+        isCorrect = true;
+        }        
+        else
+        {
+        Console.WriteLine("Error. Enter correct data.");
+        }        
+    }
+    return result;
+}
+
+double GetDistanceInSpace(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+}
+
+double x1 = GetNumber("Enter x of point A:");
+double y1 = GetNumber("Enter y of point A:");
+double z1 = GetNumber("Enter z of point A:");
+double x2 = GetNumber("Enter x of point B:");
+double y2 = GetNumber("Enter y of point B:");
+double z2 = GetNumber("Enter z of point B:");
+
+double distance = Math.Round(GetDistanceInSpace(x1, y1, z1, x2, y2, z2), 2);
+
+Console.WriteLine($"Distanse between A ({x1}, {y1}, {z1}) and B ({x2}, {y2}, {z2}) is {distance}.");
